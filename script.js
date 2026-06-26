2e497c72f627d29da0d353d7b9c61cf8c0a953e7
lovetn1123 <lovetn1123@gmail.com>
2026-06-26 17:49:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 const scenes = document.querySelectorAll(".scene");
 
-// Hiện Scene 1 khi mở web
-document.getElementById("scene1").classList.add("active");
+function showScene(id) {
+    scenes.forEach(scene => scene.classList.remove("active"));
+    document.getElementById(id).classList.add("active");
+}
+
+// Hiện Scene 1
+showScene("scene1");
+
+// Sau 8 giây chuyển sang Scene 2
+setTimeout(() => {
+    showScene("scene2");
+}, 8000);
