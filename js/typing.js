2d418c56b327d5a6fcc10e54f4bbf5d6d57514de
lovetn1123 <lovetn1123@gmail.com>
2026-06-26 14:46:27 +0700

    Update typing.js

--- js/typing.js
+++ js/typing.js
@@ -1,1 +1,53 @@
+const message = `Khi kim đồng hồ điểm 22 giờ đêm ngày 03 tháng 07...
 
+Bạn đã được triệu hồi tới Làng Hờ La 32.
+
+Để tham dự một bữa tiệc dưới ánh trăng máu.
+
+Nếu từ chối...
+
+Ma Sói sẽ nuốt chửng bạn.`;
+
+const typing = document.getElementById("typing");
+
+let i = 0;
+
+function typeText() {
+
+    if(document.getElementById("scene3").classList.contains("active")){
+
+        if(i < message.length){
+
+            typing.innerHTML += message.charAt(i);
+
+            i++;
+
+            setTimeout(typeText,35);
+
+        }else{
+
+            document.getElementById("choices").style.display="block";
+
+        }
+
+    }
+
+}
+
+const observer = new MutationObserver(()=>{
+
+    if(document.getElementById("scene3").classList.contains("active")){
+
+        typing.innerHTML="";
+
+        i=0;
+
+        typeText();
+
+    }
+
+});
+
+observer.observe(document.getElementById("scene3"),{
+    attributes:true
+});
